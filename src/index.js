c0b9d9d3593946b682f349506d9bb835a985a9a7
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-10-24 13:51:02 +0300

    feat: add random char

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './components/app/App';
 import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
-const marvelServise = new MarvelService();
-marvelServise.getCharecter(1011052).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
-
-
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=65650b7abc66b09a5feb529fb6327992';
+
+
     getResource = async(url) => {
         let res = await fetch(url);
 
@@ -11,13 +13,24 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharecters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=65650b7abc66b09a5feb529fb6327992');
+    getAllCharecters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharecter = (id) => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters?${id}?
-        apikey=65650b7abc66b09a5feb529fb6327992`)
+    getCharecter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharacter(res.data.results[0])
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
     }
 
 }

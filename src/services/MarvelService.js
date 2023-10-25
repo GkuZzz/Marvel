a417aeb43570c71cd4d9d9db4f0c94ad2596fff8
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-10-25 13:36:48 +0300

    feat: add spinner and error message

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,9 +24,10 @@ class MarvelService {
     }
 
     _transformCharacter = (char) => {
+       
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? char.description.slice(0, 150) : 'К сожалению, описания нет',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
